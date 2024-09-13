52457f67d1babe6705c4d7c6f5b75f009db44f11
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-09-13 22:23:10 +0400

    Add files via upload

--- ControlledInput.tsx
+++ ControlledInput.tsx
@@ -2,13 +2,16 @@ import { useState } from "react"
 
 export default function ControlledInput() {
     const[name,setName] = useState('')
+    
   return (
     <div>
       <input
        type="text"
         name="name"
-        
+        value={name}
+        onChange={e=> setName(e.target.value)}
         />
+        <button onClick={()=>setName('')} >submit</button>
     </div>
   )
 }
