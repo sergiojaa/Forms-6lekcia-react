b8de97ffbf2e55d957b7c49853b3c03e9f3f3ca2
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-09-14 22:09:46 +0400

    Add files via upload

--- Seminari2.tsx
+++ Seminari2.tsx
@@ -1,22 +1,34 @@
-import { SetStateAction, useState } from "react"
+import {  useState } from "react"
 export default function Seminari2() {
   const [name,setName] = useState(localStorage.getItem("name"||""))
-  const  handleChange= (e: { target: { value: SetStateAction<string> } })=>{
-      setName(e.target.value);
-      localStorage.setItem("name", e.target.value)
-  }
+  const handleChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+   ) => {
+    setName(e.target.value);
+    localStorage.setItem(
+      "name",
+       e.target.value
+      );
+  };
   const handleClick = ()=>{
+    
     setName("")
     localStorage.removeItem('name')
   }
   return (
     <div>
       seminari 2
-      <input
+      {/* <input
        value={name}
         onChange={handleChange}
-         />
-         <button onClick={handleClick} >clear</button>
+         /> */}
+         <button  onClick={handleClick} >clear</button>
+         <select value={name} onChange={handleChange} style={{width:"200px"}}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+         </select>
     </div>
   )
 }
